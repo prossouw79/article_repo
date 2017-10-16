225ff13afac10663bea85c50ade518f45c4b23f5
prossouw79 <prossouw79@gmail.com>
2017-10-16 16:36:02 +0200

    linpack implementation

--- gimmeData.sh
+++ gimmeData.sh
@@ -2,6 +2,10 @@
 target_dir="article_results"
 mkdir $target_dir -p
 
+#linpack details
+linpack_exec="~/article_repo/mpi_benchmarks/linpack/xhpl"
+linpack_data="~/article_repo/mpi_benchmarks/linpack/HPL.dat"
+
 #Physical setup
 suffix="atlantis"
 distance="CLOSE"
@@ -82,6 +86,10 @@ for wifi_ip_1 in $wifi_lines
 					fi
 		done
 	done
+	echo "#HPL_HPL_HPL_HPL_HPL_HPL_HPL_HPL_HPL_HPL_HPL_HPL_HPL_HPL#" >> $outfile
+	echo "Running LINPACK PERFORMANCE TEST..."
+	mpirun -f $machinefile -n 12 $linpack_exec $linpack_data | tee -a $outfile
+
 cat $outfile
 
 #compile parser & run on dataset
